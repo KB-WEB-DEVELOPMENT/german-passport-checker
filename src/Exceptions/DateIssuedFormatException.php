819522a129992bdedc9557ea9655a75e422c00a4
Kami Barut-Wanayo <kamibarut@yahoo.com>
2025-03-31 22:29:38 +0200

    Add files via upload

---
+++ src/Exceptions/DateIssuedFormatException.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Kbarut\TravelClearing\Exceptions;
+
+class DateIssuedFormatException extends Exception
+{
+	public string $error = "The '$dateIssued' string method param is wrongly formatted. The correct date format is 'dd-mm-yyyy'. ";
+	
+	public function printErrorMessage(): string
+    {		
+		return $this->error;
+	}
+}
---
+++ src/Exceptions/InvalidDateIssuedException.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Kbarut\TravelClearing\Exceptions;
+
+class InvalidDateIssuedException extends Exception
+{
+	public string $error = "The '$dateIssued' string method param date must lie between '01-11-2007' included and today included.";
+	
+	public function printErrorMessage(): string
+    {
+		return $this->error;	
+	}
+}

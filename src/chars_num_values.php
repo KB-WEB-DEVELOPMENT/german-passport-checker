fefd7eabfc335401726e3980dec8214ea3e16beb
Kami Barut-Wanayo <kamibarut@yahoo.com>
2025-03-31 23:07:55 +0200

    Update chars_num_values.php

--- src/chars_num_values.php
+++ src/chars_num_values.php
@@ -1,112 +1,112 @@
 <?php    
 
 return array(
-	array(
-		"character"=>"0",
- 		"numerical_value"=> 0,
-	),
-    array(
-		"character"=>"1",
- 		"numerical_value"=> 1,
-	),
-	array(
-		"character"=>"2",
- 		"numerical_value"=> 2,
-	),
-	array(
-		"character"=>"3",
- 		"numerical_value"=> 3,
-	),
-	array(
-		"character"=>"4",
- 		"numerical_value"=> 4,
-	),
-	array(
-		"character"=>"5",
- 		"numerical_value"=> 5,
-	),
-	array(
-		"character"=>"6",
- 		"numerical_value"=> 6,
-	),
-	array(
-		"character"=>"7",
- 		"numerical_value"=> 7,
-	),
-	array(
-		"character"=>"8",
- 		"numerical_value"=> 8,
-	),
-	array(
-		"character"=>"9",
- 		"numerical_value"=> 9,
-	),   
-    array(
-		"character"=>"C",
- 		"numerical_value"=> 12,
-	),
-    array(
-		"character"=>"F",
- 		"numerical_value"=> 15,
-	),
-	array(
-		"character"=>"G",
- 		"numerical_value"=> 16,
-	),
-	array(
-		"character"=>"H",
- 		"numerical_value"=> 17,
-	),
-	array(
-		"character"=>"J",
- 		"numerical_value"=> 19,
-	),
-	array(
-		"character"=>"K",
- 		"numerical_value"=> 20,
-	),
-	array(
-		"character"=>"L",
- 		"numerical_value"=> 21,
-	),
-	array(
-		"character"=>"M",
- 		"numerical_value"=> 22,
-	),
-	array(
-		"character"=>"N",
- 		"numerical_value"=> 23,
-	),
-	array(
-		"character"=>"P",
- 		"numerical_value"=> 25,
-	),
-	array(
-		"character"=>"R",
-		"numerical_value"=> 27,
-	),
-	array(
-		"character"=>"T",
-		"numerical_value"=> 29,
-	),
-	array(
-		"character"=>"V",
-		"numerical_value"=> 31,
-	),
-	array(
-		"character"=>"W",
-		"numerical_value"=> 32,
-	),
-	array(
-		"character"=>"X",
-		"numerical_value"=> 33,
-	),
-	array(
-		"character"=>"Y",
-		"numerical_value"=> 34,
-	),
-	array(
-		"character"=>"Z",
-		"numerical_value"=> 35,
-	),	
+   array(
+     "character"=>"0",
+     "numerical_value"=> 0,
+   ),
+   array(
+     "character"=>"1",
+     "numerical_value"=> 1,
+   ),
+   array(
+     "character"=>"2",
+     "numerical_value"=> 2,
+   ),
+   array(
+     "character"=>"3",
+     "numerical_value"=> 3,
+   ),
+   array(
+     "character"=>"4",
+     "numerical_value"=> 4,
+   ),
+   array(
+     "character"=>"5",
+     "numerical_value"=> 5,
+   ),
+   array(
+     "character"=>"6",
+     "numerical_value"=> 6,
+   ),
+   array(  
+      "character"=>"7",
+      "numerical_value"=> 7,
+   ),
+   array(
+      "character"=>"8",
+      "numerical_value"=> 8,
+   ),
+   array(
+      "character"=>"9",
+      "numerical_value"=> 9,
+   ),   
+   array(
+      "character"=>"C",
+      "numerical_value"=> 12,
+   ),
+   array(
+      "character"=>"F",
+      "numerical_value"=> 15,
+   ),
+   array(
+      "character"=>"G",
+      "numerical_value"=> 16,
+   ),
+   array(
+      "character"=>"H",
+       "numerical_value"=> 17,
+   ),
+   array(
+      "character"=>"J",
+      "numerical_value"=> 19,
+   ),
+   array(
+      "character"=>"K",
+      "numerical_value"=> 20,
+   ),
+   array(
+      "character"=>"L",
+      "numerical_value"=> 21,
+   ),
+   array(
+      "character"=>"M",
+      "numerical_value"=> 22,
+   ),
+   array(
+      "character"=>"N",
+      "numerical_value"=> 23,
+   ),
+   array(
+      "character"=>"P",
+      "numerical_value"=> 25,
+   ),
+   array(
+      "character"=>"R",
+      "numerical_value"=> 27,
+   ),
+   array(
+      "character"=>"T",
+      "numerical_value"=> 29,
+   ),
+   array(
+      "character"=>"V",
+      "numerical_value"=> 31,
+   ),
+   array(
+      "character"=>"W",
+      "numerical_value"=> 32,
+   ),
+   array(
+      "character"=>"X",
+      "numerical_value"=> 33,
+   ),
+   array(
+      "character"=>"Y",
+      "numerical_value"=> 34,
+   ),
+   array(
+      "character"=>"Z",
+      "numerical_value"=> 35,
+   ),	
 );

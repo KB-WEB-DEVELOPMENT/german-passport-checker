e7bc0cb15d58f4f855957657ba8279ad1eac4fec
Kami Barut-Wanayo <kamibarut@yahoo.com>
2025-03-31 22:35:49 +0200

    Update InvalidDateIssuedException.php

--- src/Exceptions/InvalidDateIssuedException.php
+++ src/Exceptions/InvalidDateIssuedException.php
@@ -4,10 +4,10 @@ namespace Kbarut\TravelClearing\Exceptions;
 
 class InvalidDateIssuedException extends Exception
 {
-	public string $error = "The '$dateIssued' string method param date must lie between '01-11-2007' included and today included.";
+    public string $error = "The '$dateIssued' string method param date must lie between '01-11-2007' included and today included.";
 	
-	public function printErrorMessage(): string
+    public function printErrorMessage(): string
     {
-		return $this->error;	
-	}
+	return $this->error;	
+    }
 }

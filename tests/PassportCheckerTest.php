a0f54d81cd2dd1eb4dc7da4f34f17d5fdbc70eb8
Kami Barut-Wanayo <kamibarut@yahoo.com>
2025-03-31 22:32:54 +0200

    Update PassportCheckerTest.php

--- tests/PassportCheckerTest.php
+++ tests/PassportCheckerTest.php
@@ -8,14 +8,11 @@ it('empty passport number input', function () {
 	
     $passportChecker = Mockery::mock(PassportChecker::class);
 	
-	$res = $passportChecker->validate('','29-03-2025')
+    $res = $passportChecker->validate('','29-03-2025')
 	
-	expect($res)->toBeFalse();
-    
+    expect($res)->toBeFalse();
 });
 
 /*
-
 Create  and run similar tests based on PassportChecker methods outputss ...
-
 */

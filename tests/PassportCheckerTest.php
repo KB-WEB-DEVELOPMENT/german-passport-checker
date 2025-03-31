6ab4795ddcab2186b5c3819ce461b36da8279980
Kami Barut-Wanayo <kamibarut@yahoo.com>
2025-03-31 22:32:02 +0200

    Create PassportCheckerTest.php

---
+++ tests/PassportCheckerTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Kbarut\TravelClearing\Tests;
+
+use Kbarut\TravelClearing\PassportChecker;
+
+it('empty passport number input', function () {
+	
+    $passportChecker = Mockery::mock(PassportChecker::class);
+	
+	$res = $passportChecker->validate('','29-03-2025')
+	
+	expect($res)->toBeFalse();
+    
+});
+
+/*
+
+Create  and run similar tests based on PassportChecker methods outputss ...
+
+*/
